de7cb97bdae3a35173228ff0ec6f895471935bce
AlvinBach <quocanh1990bach@gmail.com>
2021-12-18 14:27:04 +0200

    workout schedular app with Angular

---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatInputModule],
+  exports: [MatButtonModule, MatDividerModule, MatIconModule, MatInputModule],
+})
+export class MaterialModule {}
